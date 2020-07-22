5021dc33cd7b1574696ed7ac114f140ac89ee22a
dbrauerh <40126100+dbrauerh@users.noreply.github.com>
2020-07-21 23:47:31 -0400

    Actualizando códigos
    Al tener error en programa y ver que colab me decia que habían códigos obsoletos intento actualizarlos

--- codigos_colab/generate_tfrecord.py
+++ codigos_colab/generate_tfrecord.py
@@ -83,7 +83,7 @@ def create_tf_example(group, path):
 
 
 def main(_):
-    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
@@ -97,4 +97,4 @@ def main(_):
 
 
 if __name__ == '__main__':
-    tf.app.run()
+    tf.compat.v1.app.run
