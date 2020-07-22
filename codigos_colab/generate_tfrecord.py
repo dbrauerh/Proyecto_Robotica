88bb821c50e9e3b1b0a35b6c96328697776d9093
dbrauerh <40126100+dbrauerh@users.noreply.github.com>
2020-07-21 23:29:52 -0400

    Cambio por tutorial medium
    Al ver problema estoy intentando de copiar el tutorial de medium

--- codigos_colab/generate_tfrecord.py
+++ codigos_colab/generate_tfrecord.py
@@ -22,13 +22,15 @@ from collections import namedtuple, OrderedDict
 flags = tf.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_string('label', '', 'Name of class label')
+flags.DEFINE_string('label0', '', 'Name of class[0] label')
 flags.DEFINE_string('image_dir', '', 'Path to images')
 FLAGS = flags.FLAGS
 
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'semaforo':
+    if row_label == FLAGS.label: # 'semaforo':
         return 1
     else:
         None
